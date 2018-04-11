b036790be73efc7f66963ccdfb5e9b674fe67b92
albin <padinju@gmail.com>
2018-04-11 22:55:37 +0530

    php login form

---
+++ login_form/index.php
@@ -0,0 +1,30 @@
+
+
+
+<html>
+<head>
+	<title>Go</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+	<div class="row">
+	    <div class="col-sm-6 col-md-6 col-lg-6" style="background-color:white;text-align: center;height:100%;">
+				<div><h1 style="font-size:90px;text-align:right;">G</h1></div>
+				<div style="padding:30%;"><a href="/signup.php" style="color:black;font-size:70px">Sign Up</a></div>
+	    </div>
+	    <div class="col-sm-6 col-md-6 col-lg-6" style="background-color:black;text-align: center;height:100%;">
+				<div><h1 style="font-size:90px;text-align:left;color:white;">O</h1></div>
+				<div style="padding:30%;"><a href="/signin.php" style="color:white;font-size:70px;">Sign In</a></div>
+	    </div>
+	  </div>
+
+</body>
+</html>
